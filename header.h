21592cdd4034ab69b58dca9e6eff03f02205eeb9
km93120 <35903578+km93120@users.noreply.github.com>
2018-01-30 22:19:28 +0100

    Add files via upload

--- header.h
+++ header.h
@@ -1,49 +1,174 @@
 #include <iostream>
+#include <cstdlib>
+#include <new>
+#include <cmath>
 using namespace std;
-class Vehicule
+void echange(int &,int &);
+void echange(int &x,int &y)
 {
-private:
-    string marque;
-    string modele;
-    int cylindree;
-    string matricule;
-    float prix;
+    int temp = 0;
+    temp = x;
+    x = y;
+    y = temp;
+
+
+
+}
+
+
+struct essai
+{
+    int n;
+    float x;
+
+
+}e;
+void setValue(struct essai *);
+void setValue(struct essai *str)
+{
+int m,p;
+cout << "valeurs de n et x?"<<"\n";
+cin >> m;
+cin >> p;
+str->n = m;
+str->x = p;
+cout << m << "\t" << p;
+}
+void raz(struct essai *);
+void raz(struct essai *str)
+{
+cout << "remise a 0 des attributs de l'objet e"<<"\n";
+str->n = 0;
+str->x = 0;
+
+}
+new_handler depassement();
+new_handler depassement()
+{
+    cout << "depassement de memoire!!!";
+    exit(1);
+
+}
+
+
+/*class Point
+{
+    float x;
+    float y;
 public:
-    void afficher();
-    void saisir();
-     Vehicule (string,string,int,string,float);
+    void deplace();
+    void affiche();
+    Point(float,float);
+
 
 };
 
-Vehicule::Vehicule(string ma,string mo,int cyl,string mat,float pr)
+void Point::affiche()
 {
-    marque = ma;
-    modele = mo;
-    cylindree = cyl;
-    matricule = mat;
-    prix = pr;
+    cout << x<<"\n"<<y;
+
 }
 
-void Vehicule::afficher()
+void Point::deplace()
 {
-cout << "les caracteristiques de la voiture sont les suivantes :\n";
-cout << marque<<"\t";
-cout << modele<<"\t";
-cout << cylindree<<"\t";
-cout << matricule<<"\t";
-cout << prix<<"\t\n";
+    float x_,y_;
+    cout<< "déplacementde l'axe x ?\n";
+    cin >> x_;
+    x += x_;
+    cout << "deplacement de l axe y?\n";
+    cin >> y_;
+    y += y_;
+
 }
 
-void Vehicule::saisir()
+Point::Point(float abscisse,float ordonnee)
 {
-    cout << "marque ?";
-    cin >> marque;
-    cout <<"modele?";
-    cin >> modele;
-    cout<<"cylindree";
-    cin >> cylindree;
-    cout <<"matricule?";
-    cin >> matricule;
-    cout<< "prix?";
-    cin>>prix;
+    x = abscisse;
+    y = ordonnee;
+
+}*/
+
+float angle(float,float);
+float angle(float x,float y)
+{
+    int ang = atan(y/x);
+    return ang;
 }
+class Point
+{
+    float x;
+    float y;
+public:
+    void deplace();
+    float get_abscisse();
+    float get_ordonnee();
+    void homothetie(float);
+    void rotation(float);
+    float rho();
+    float theta();
+
+    Point(float,float);
+
+
+};
+Point::Point(float abscisse,float ordonnee)
+{
+    x = abscisse;
+    y = ordonnee;
+}
+float Point :: get_abscisse()
+{
+    return(x);
+
+}
+
+float Point :: get_ordonnee()
+{
+    return(y);
+
+}
+
+void Point::deplace()
+{
+    float x_,y_;
+    cout<< "déplacementde l'axe x ?\n";
+    cin >> x_;
+    x += x_;
+    cout << "deplacement de l axe y?\n";
+    cin >> y_;
+    y += y_;
+
+}
+
+void Point :: homothetie(float k)
+{
+    x *= k;
+    y *= k;
+}
+
+void Point :: rotation(float angl)
+{
+
+float r = sqrt(x*x + y*y);
+angl += angle(x,y);
+x = r*cos(angl);
+y = r*sin(angl);
+
+}
+
+float Point ::rho()
+{
+    return(sqrt(x*x + y*y));
+
+}
+
+float Point :: theta()
+{
+    return(atan(y/x));
+}
+
+
+
+
+
+
--- main.cpp
+++ main.cpp
@@ -1,9 +1,73 @@
 #include "header.h"
 using namespace std;
-main()
+
+int main()
 {
-    Vehicule v("Opel","Grandland X",300,"rm76C",20000);
-    v.afficher();
-    v.saisir();
-    v.afficher();
+   /* int a,b;
+    cin >> a;
+    cin >> b;
+    echange(a,b);
+    cout <<a <<"\n"<< b<<"\n";
+
+    setValue(&e);
+    raz(&e);
+
+    int *adi;
+    double *add;
+    adi = new int;
+    add = new double[100];
+
+    double *adtab;
+    int nval;
+    cout << "nombre de valeurs?";
+    cin >> nval;
+    adtab = new double[nval];*/
+
+
+    /*int tailleTableau;
+    int numeroBloc;
+    cin >> tailleTableau;
+    int *ptr = NULL;
+    for(numeroBloc = 0; ; numeroBloc++)
+    {
+        ptr = new int[tailleTableau];
+        if (!ptr)
+        {cout<<"allocation du bloc" << numeroBloc << "a echoue";
+            exit(1);}
+        else
+            cout << "allocation du bloc" << numeroBloc << "faite avec succes";
+
+    }*/
+
+    /*Point p(5,3);
+    p.affiche();
+    p.deplace();
+    p.affiche();*/
+
+
+    /*Point p(5,3);
+   cout << "abscisse : "<<p.get_abscisse()<<"\n";
+   cout << "ordonnee : "<<p.get_ordonnee()<<"\n";
+    p.deplace();
+   cout << "abscisse : "<<p.get_abscisse()<<"\n";
+   cout << "ordonnee : "<<p.get_ordonnee()<<"\n";
+    p.homothetie(2);
+    cout<<"apres homothétie\n";
+   cout << "abscisse : "<<p.get_abscisse()<<"\n";
+   cout << "ordonnee : "<<p.get_ordonnee()<<"\n";
+    p.rotation(50);
+    cout <<"apres rotation\n";
+    cout << "abscisse : "<<p.get_abscisse()<<"\n";
+    cout << "ordonnee : "<<p.get_ordonnee()<<"\n";
+
+   cout << "module "<<p.rho()<<"\n";
+   cout << "argument"<<p.theta()<<"\n";*/
+
+
+
+
+
+
+
 }
+
